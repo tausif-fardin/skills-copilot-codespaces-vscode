d56fc0172d7804c016c1fff680821639c216975c
Tausif Fardin Sinha <tausiffardin4@gmail.com>
2024-10-14 17:42:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember(){
+    var skills = [
+        {name: 'HTML', level: 8},
+        {name: 'CSS', level: 7},
+        {name: 'Javascript', level: 6},
+        {name: 'PHP', level: 5},
+        {name: 'Python', level: 4},
+    ];
+    return skills;
+}
